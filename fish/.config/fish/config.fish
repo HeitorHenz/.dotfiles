b8edfa364340e3e442461392cd95c9414b898da8
H <heitor1954hg@gmail.com>
2026-09-23 11:30:23 -0300

    progress

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,3 +9,10 @@ end
 
 # uv
 fish_add_path "/var/home/h/.local/bin"
+
+# pnpm
+set -gx PNPM_HOME "/home/h/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
